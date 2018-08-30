a91a46b6acb3644c2ce67fbdcb6b8795fb3da2ec
fuqunaga <fukhid@gmail.com>
2018-08-30 19:02:49 +0900

    first commit

---
+++ Assets/PortalGate/PortalGate.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace PortalGateSystem
+{
+    public class PortalGate : MonoBehaviour
+    {
+        public PortalGate pair;
+        Quaternion rotY = Quaternion.Euler(0f, 180f, 0f);
+
+        Dictionary<Camera, Camera> cameraTable = new Dictionary<Camera, Camera>();
+
+
+        Material material;
+
+        private void Start()
+        {
+            var renderer = GetComponent<Renderer>();
+            material = renderer.material;
+        }
+
+        private void OnDestroy()
+        {
+            Destroy(material);
+        }
+
+        private void OnWillRenderObject()
+        {
+            var cam = Camera.current;
+
+            var isPair = cameraTable.ContainsValue(cam);
+
+            if (!isPair)
+            {
+                var camTrans = cam.transform;
+
+                var localPos = transform.InverseTransformPoint(camTrans.position);
+                var localRot = Quaternion.Inverse(transform.rotation) * camTrans.rotation;
+
+                var pairTrans = pair.transform;
+                var pos = pairTrans.TransformPoint(rotY * localPos);
+                var rot = pairTrans.rotation * rotY * localRot;
+
+                var pairCam = GetPairCamera(cam);
+                pairCam.transform.SetPositionAndRotation(pos, rot);
+
+                material.mainTexture = pairCam.targetTexture;
+            }
+        }
+
+        Camera GetPairCamera(Camera cam)
+        {
+            Camera ret;
+            if (!cameraTable.TryGetValue(cam, out ret))
+            {
+                ret = cameraTable[cam] = CreatePairCamera(cam);
+            }
+
+            return ret;
+        }
+
+        Camera CreatePairCamera(Camera cam)
+        {
+            var go = new GameObject(cam.name + "_pair");
+            go.transform.SetParent(transform);
+
+            var c = go.AddComponent<Camera>();
+            c.CopyFrom(cam);
+            c.depth = cam.depth - 1;
+
+            var baseTex = cam.targetTexture;
+            var size = (baseTex != null)
+                ? new Vector2Int(baseTex.width, baseTex.height)
+                : new Vector2Int(Screen.width, Screen.height);
+
+            var tex = new RenderTexture(size.x, size.y, 24);
+
+            c.targetTexture = tex;
+
+            return c;
+        }
+    }
+}
---
+++ Assets/PortalGate/PortalGate.shader
@@ -0,0 +1,51 @@
+﻿Shader "Custom/PortalGate"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			struct v2f
+			{
+				float4 pos : SV_POSITION;
+				float4 spos : TEXCOORD0;
+				//half2 uv : TEXCOORD0;
+			};
+
+			v2f vert( appdata_img v )
+			{
+				v2f o;
+
+				o.pos = UnityObjectToClipPos (v.vertex);
+				o.spos = ComputeScreenPos(o.pos);
+				//o.uv = v.texcoord;
+				return o;
+			}
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float2 sUv = i.spos.xy / i.spos.w;
+				//float4 col = float4(sUv, 0, 1);
+				float4 col = tex2D(_MainTex, sUv);
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
