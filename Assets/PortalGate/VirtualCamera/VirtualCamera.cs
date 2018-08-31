2f1a904453ffd81c1e85e52c70eba7e09b64bc28
fuqunaga <fukhid@gmail.com>
2018-08-31 19:23:25 +0900

    Virtual Cameraの手前描画を弾く。nearClipでお手軽実装

--- Assets/PortalGate/VirtualCamera/VirtualCamera.cs
+++ Assets/PortalGate/VirtualCamera/VirtualCamera.cs
@@ -1,14 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace PortalGateSystem
 {
     [RequireComponent(typeof(Camera))]
     public class VirtualCamera : MonoBehaviour
     {
-        public Transform zBarrier;
-
         public Camera cam;
 
         public PortalGate parentGate;
@@ -33,21 +29,30 @@ namespace PortalGateSystem
             cam.targetTexture = tex;
         }
 
-
         private void LateUpdate()
         {
             var parentGateTrans = parentGate.transform;
             var parentCamTrans = parentCamera.transform;
-            var pairGateTrans = parentGate.pair.transform;
 
             var localPos = parentGateTrans.InverseTransformPoint(parentCamTrans.position);
             var localRot = Quaternion.Inverse(parentGateTrans.rotation) * parentCamTrans.rotation;
 
-            var gateRot = parentGate.gateRot;
+            var pairGate = parentGate.pair;
+            SetPairLocalToTransform(transform, pairGate, localPos, localRot);
+
+            // TODO: pairGateが斜めってるとき正しくない
+            var pairGatePosOnCamera = transform.InverseTransformPoint(pairGate.transform.position);
+            cam.nearClipPlane = Mathf.Max(0f, pairGatePosOnCamera.z - 1f);
+        }
+
+        void SetPairLocalToTransform(Transform trans, PortalGate pairGate, Vector3 localPos, Quaternion localRot)
+        {
+            var pairGateTrans = pairGate.transform;
+            var gateRot = pairGate.gateRot;
             var pos = pairGateTrans.TransformPoint(gateRot * localPos);
             var rot = pairGateTrans.rotation * gateRot * localRot;
 
-            transform.SetPositionAndRotation(pos, rot);
+            trans.SetPositionAndRotation(pos, rot);
         }
     }
 }
