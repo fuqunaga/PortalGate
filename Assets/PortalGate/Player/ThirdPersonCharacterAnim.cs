694b4f4ad8a84633e4c646470d36d0901af6bb6e
fuqunaga <fukhid@gmail.com>
2018-09-18 17:49:47 +0900

    add postprocessing stack

--- Assets/PortalGate/Player/ThirdPersonCharacterAnim.cs
+++ Assets/PortalGate/Player/ThirdPersonCharacterAnim.cs
@@ -8,20 +8,20 @@ namespace PortalGateSystem
     {
 		[SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
 		[SerializeField] float m_AnimSpeedMultiplier = 1f;
-		[SerializeField] float m_GroundCheckDistance = 0.1f;
+		//[SerializeField] float m_GroundCheckDistance = 0.1f;
 
 		[SerializeField] Rigidbody m_Rigidbody;
 		Animator m_Animator;
 		bool m_IsGrounded;
 		//float m_OrigGroundCheckDistance;
 		const float k_Half = 0.5f;
-		float m_TurnAmount;
+		float m_TurnAmount = 0f;
 		float m_ForwardAmount;
-		Vector3 m_GroundNormal;
+		Vector3 m_GroundNormal = Vector3.up;
         //float m_CapsuleHeight;
         //Vector3 m_CapsuleCenter;
         //CapsuleCollider m_Capsule;
-        bool m_Crouching;
+        bool m_Crouching = false;
 
         [SerializeField] CharacterController m_charaController;
 
